9736b3ac8442434d43166139f0bb1112b7cd6931
Claudio Cavalli <dark_claudio@hotmail.it>
2020-03-05 16:59:46 +0100

    Initial Commit

---
+++ MVVM+ReactiveCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM+ReactiveCoordinator
+//
+//  Created by claudio cavalli on 05/03/2020.
+//  Copyright © 2020 claudio cavalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
